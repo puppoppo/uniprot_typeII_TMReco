8036ef242e0ba87c79ac509a5ec6fb9cd7392bc7
RyoheiW <ryohei.a.watanabe@gmail.com>
2021-07-30 16:07:20 +0900

    no message

--- 古賀ちゃんへ/last7.pl
+++ 古賀ちゃんへ/last7.pl
@@ -1,10 +1,16 @@
 
-open(SWISS, "typeII.dat");
-open(WRITE,">gol.csv");
+open(SWISS, "uniprot_sprot.dat");
+open(WRITE,">t2_202103.csv");
 
 $, = ",";
 $\ = "\n";
 $U=0;	#配列にU,Xが含まれているかフラグ化
+
+$t2eco=0;
+$pmeco=0;
+$goleco=0;
+$ereco=0;
+$iso=0;
 
 while(<SWISS>){
 	chomp;
@@ -221,98 +227,102 @@ while(<SWISS>){
 				}
 
 
-				if($t2eco!=255 && $goleco!=0 && $goleco!=1 && $goleco!=255){
+				if($t2eco == 269 || $t2eco==303 || $t2eco==305 || $t2eco==250){
 
-					for($i=0;$i<@sq;$i++){
-						if($sq[$i] =~ /A/){
-							$sq[$i] = 1.8;
-						}
-						elsif($sq[$i] =~ /C/){
-							$sq[$i]=2.5;
-						}
-						elsif($sq[$i] =~ /D/){
-							$sq[$i]=-3.5;
-						}
-						elsif($sq[$i] =~ /E/){
-							$sq[$i]=-3.5;
-						}
-						elsif($sq[$i] =~ /F/){
-							$sq[$i]=2.8;
-						}
-						elsif($sq[$i] =~ /G/){
-							$sq[$i] = -0.4;
-						}
-						elsif($sq[$i] =~ /H/){
-							$sq[$i]=-3.2;
-						}
-						elsif($sq[$i] =~ /I/){
-							$sq[$i]=4.5;
-						}
-						elsif($sq[$i] =~ /K/){
-							$sq[$i]=-3.9;
-						}
-						elsif($sq[$i] =~ /L/){
-							$sq[$i]=3.8;
-						}
-						elsif($sq[$i] =~ /M/){
-							$sq[$i]=1.9;
-						}
-						elsif($sq[$i] =~ /N/){
-							$sq[$i]=-3.5;
-						}
-						elsif($sq[$i] =~ /P/){
-							$sq[$i]=-1.6;
-						}
-						elsif($sq[$i] =~ /Q/){
-							$sq[$i]=-3.5;
-						}
-						elsif($sq[$i] =~ /R/){
-							$sq[$i]=-4.5;
-						}
-						elsif($sq[$i] =~ /S/){
-							$sq[$i]=-0.8;
-						}
-						elsif($sq[$i] =~ /T/){
-							$sq[$i]=-0.7;
-						}
-						elsif($sq[$i] =~ /V/){
-							$sq[$i]=4.2;
-						}
-						elsif($sq[$i] =~ /W/){
-							$sq[$i]=-0.9;
-						}
-						elsif($sq[$i] =~ /Y/){
-							$sq[$i]=-1.3;
-						}
-						else{
-							printf "error" . $swissid . $i. "\n";
-						}
-					}
-
-					$max = 0;
-					@hydra = (0);
-
-					for($i=0;$i<@sq;$i++){
-						for($j=-7;$j<=7;$j++){
-							if($i+$j < 0){$hydra[$i]+= $A;} #無いところを＄A=0で置き換え
-							elsif($i+$j >= 0){$hydra[$i]+= $sq[$i+$j];}
-						}
-						if($hydra[$max]<$hydra[$i]){
-							$max=$i;
-						}
-					}
+					# for($i=0;$i<@sq;$i++){
+					# 	if($sq[$i] =~ /A/){
+					# 		$sq[$i] = 1.8;
+					# 	}
+					# 	elsif($sq[$i] =~ /C/){
+					# 		$sq[$i]=2.5;
+					# 	}
+					# 	elsif($sq[$i] =~ /D/){
+					# 		$sq[$i]=-3.5;
+					# 	}
+					# 	elsif($sq[$i] =~ /E/){
+					# 		$sq[$i]=-3.5;
+					# 	}
+					# 	elsif($sq[$i] =~ /F/){
+					# 		$sq[$i]=2.8;
+					# 	}
+					# 	elsif($sq[$i] =~ /G/){
+					# 		$sq[$i] = -0.4;
+					# 	}
+					# 	elsif($sq[$i] =~ /H/){
+					# 		$sq[$i]=-3.2;
+					# 	}
+					# 	elsif($sq[$i] =~ /I/){
+					# 		$sq[$i]=4.5;
+					# 	}
+					# 	elsif($sq[$i] =~ /K/){
+					# 		$sq[$i]=-3.9;
+					# 	}
+					# 	elsif($sq[$i] =~ /L/){
+					# 		$sq[$i]=3.8;
+					# 	}
+					# 	elsif($sq[$i] =~ /M/){
+					# 		$sq[$i]=1.9;
+					# 	}
+					# 	elsif($sq[$i] =~ /N/){
+					# 		$sq[$i]=-3.5;
+					# 	}
+					# 	elsif($sq[$i] =~ /P/){
+					# 		$sq[$i]=-1.6;
+					# 	}
+					# 	elsif($sq[$i] =~ /Q/){
+					# 		$sq[$i]=-3.5;
+					# 	}
+					# 	elsif($sq[$i] =~ /R/){
+					# 		$sq[$i]=-4.5;
+					# 	}
+					# 	elsif($sq[$i] =~ /S/){
+					# 		$sq[$i]=-0.8;
+					# 	}
+					# 	elsif($sq[$i] =~ /T/){
+					# 		$sq[$i]=-0.7;
+					# 	}
+					# 	elsif($sq[$i] =~ /V/){
+					# 		$sq[$i]=4.2;
+					# 	}
+					# 	elsif($sq[$i] =~ /W/){
+					# 		$sq[$i]=-0.9;
+					# 	}
+					# 	elsif($sq[$i] =~ /Y/){
+					# 		$sq[$i]=-1.3;
+					# 	}
+					# 	else{
+					# 		printf "error" . $swissid . $i. "\n";
+					# 	}
+					# }
+					#
+					# $max = 0;
+					# @hydra = (0);
+					#
+					# for($i=0;$i<@sq;$i++){
+					# 	for($j=-7;$j<=7;$j++){
+					# 		if($i+$j < 0){$hydra[$i]+= $A;} #無いところを＄A=0で置き換え
+					# 		elsif($i+$j >= 0){$hydra[$i]+= $sq[$i+$j];}
+					# 	}
+					# 	if($hydra[$max]<$hydra[$i]){
+					# 		$max=$i;
+					# 	}
+					# }
 
 					@sq=();
 					@sq=split(//,$swisssq);
 
-					printf WRITE ">".$swissid.",".$t2eco.",".$pmeco.",".$goleco.",".$ereco."\n";
-					for($i=$max-25;$i<=$max+25;$i++){
-						if($i<=0 || $i>=@sq){
-							printf WRITE "X";
-						}
-						else{
-							printf WRITE $sq[$i],",";
-						}
+					printf WRITE ">".$swissid.",".$t2eco.",".$pmeco.",".$goleco.",".$ereco.",".$iso."\n";
+					# for($i=$max-25;$i<=$max+25;$i++){
+					# 	if($i<=0 || $i>=@sq){
+					# 		printf WRITE "X";
+					# 	}
+					# 	else{
+					# 		printf WRITE $sq[$i],",";
+					# 	}
+					# }
+
+					for($i=@sq-7;$i<@sq;$i++){
+						printf WRITE $sq[$i].",";
 					}
 
 					printf WRITE "\n" ;
